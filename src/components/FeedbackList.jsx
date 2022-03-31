73a375f134334174949bac8c61aa06822e8cc01c
Sunit Corneleous <sunit15-10591@diu.edu.bd>
2022-03-31 21:37:38 +0600

    json server added, spinner component added

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -3,15 +3,18 @@ import { useContext } from "react";
 import React from "react";
 import FeedbackItem from "./FeedbackItem";
 import FeedbackContext from "../context/FeedbackContext";
+import Spinner from "./shared/Spinner";
 
 function Feedbacklist() {
-  const { feedback } = useContext(FeedbackContext);
+  const { feedback, isLoading } = useContext(FeedbackContext);
 
-  if (!feedback || feedback.length === 0) {
+  if (!isLoading && (!feedback || feedback.length === 0)) {
     return <p>No Feedback Yet</p>;
   }
 
-  return (
+  return isLoading ? (
+    <Spinner />
+  ) : (
     <div className="feedback-list">
       <AnimatePresence>
         {feedback.map((item) => (
@@ -21,10 +24,7 @@ function Feedbacklist() {
             animate={{ opacity: 1 }}
             exit={{ opacity: 0 }}
           >
-            <FeedbackItem
-              key={item.id}
-              item={item}
-            />
+            <FeedbackItem key={item.id} item={item} />
           </motion.div>
         ))}
       </AnimatePresence>
